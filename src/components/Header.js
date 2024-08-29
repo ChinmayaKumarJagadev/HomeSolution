f58b2d94b38e2590879a2077bb94e167ccf4fa6f
chinmaya-elred <chinmaya@elred.io>
2024-08-29 11:33:38 +0530

    done menu changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -73,7 +73,7 @@ function Header() {
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.9 }}
             transition={{ duration: 0.3 }}
-            className="relative w-full max-w-full bg-slate-700 bg-opacity-70 shadow-xl lg:hidden text-white gap-5 overflow-auto"
+            className="relative w-full max-w-full bg-slate-700 bg-opacity-70 shadow-xl text-white gap-5 overflow-auto lg:hidden md:overflow-auto"
             style={{ height: "100%" }}
           >
             <button
@@ -83,29 +83,29 @@ function Header() {
             >
               &times;
             </button>
-            <ul className="flex flex-col space-y-10 uppercase tracking-wider mt-5">
-              <li className="hover:bg-orange-700 p-3 rounded mt-20 text-2xl m-3">
+            <ul className="flex flex-col space-y-4 uppercase tracking-wider mt-2">
+              <li className="hover:bg-orange-700 p-3 rounded mt-20 text-xl m-3">
                 <Link href="/">About</Link>
               </li>
-              <li className="hover:bg-orange-700 p-3 rounded text-2xl m-8">
+              <li className="hover:bg-orange-700 p-3 rounded text-xl m-8">
                 <Link href="/">Services</Link>
               </li>
-              <li className="hover:bg-orange-700 p-3 rounded text-2xl m-8">
+              <li className="hover:bg-orange-700 p-3 rounded text-xl m-8">
                 <Link href="/">Projects</Link>
               </li>
-              <li className="hover:bg-orange-700 p-3 rounded text-2xl m-8">
+              <li className="hover:bg-orange-700 p-3 rounded text-xl m-8">
                 <Link href="/">Testimonials</Link>
               </li>
-              <li className="hover:bg-orange-700 p-3 rounded text-2xl m-8">
+              <li className="hover:bg-orange-700 p-3 rounded text-xl m-8">
                 <Link href="/">Contact</Link>
               </li>
 
               <hr className="w-full" />
             </ul>
 
-            <div className="text-2xl mt-7 md:mt-9 ml-5 md:ml-[100px] float-left mb-5 md:mb-9">
-              <div className="text-left mb-4 md:text-3xl">NEED HELP ?</div>
-              <div className="flex flex-col gap-3 md:gap-7">
+            <div className="text-2xl mt-3 md:mt-9 ml-5 md:ml-[100px] float-left mb-5 md:mb-9">
+              <div className="text-left mb-2 md:text-3xl text-[16px] font-bold">NEED HELP ?</div>
+              <div className="flex flex-col gap-1 md:gap-7">
                 <Link
                   href="https://www.facebook.com"
                   passHref
@@ -118,7 +118,7 @@ function Header() {
                     height={30}
                     width={30}
                   />
-                  <span className="text-xl ml-6 text-orange-600 font-bold md:text-3xl">
+                  <span className="text-[18px] ml-3 text-orange-600 font-bold md:text-3xl">
                     0941 112 0710
                   </span>
                 </Link>
@@ -131,7 +131,7 @@ function Header() {
                     height={30}
                     width={30}
                   />
-                  <span className="text-xl ml-6 text-orange-600 font-bold md:text-3xl">
+                  <span className="text-[18px] ml-3 text-orange-600 font-bold md:text-3xl">
                     supportalok@gmail.com
                   </span>
                 </Link>
@@ -141,18 +141,18 @@ function Header() {
             <hr className="w-full" />
 
             <div className="md:ml-[100px] ml-4 mb-5">
-              <div className="mt-5 text-left md:text-3xl">FOLLOW US</div>
-              <div className="flex gap-5 mt-5">
+              <div className="mt-5 text-left md:text-3xl text-[16px] font-bold">FOLLOW US</div>
+              <div className="flex gap-5 mt-1" style={{ marginLeft: "-5px"}}>
               <Link href="https://www.facebook.com" passHref>
-                <FaFacebookF className={styles.scoialIcons} style={{ fontSize: "30px"}}/>
+                <FaFacebookF className={styles.scoialIcons} />
               </Link>
 
               <Link href="https://www.linkedin.com" passHref>
-                <FaLinkedinIn className={styles.scoialIcons} style={{ fontSize: "30px"}}/>
+                <FaLinkedinIn className={styles.scoialIcons} />
               </Link>
 
               <Link href="https://www.twitter.com" passHref>
-                <FaTwitter className={styles.scoialIcons} style={{ fontSize: "30px"}}/>
+                <FaTwitter className={styles.scoialIcons} />
               </Link>
               </div>
             </div>
