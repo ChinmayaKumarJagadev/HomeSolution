5f6b9a8211011185ed540ca4dc887ec4c6ec7627
chinmaya-elred <chinmaya@elred.io>
2024-09-05 10:55:56 +0530

    add new chnages for construction section

--- src/components/Home/OfferSection.js
+++ src/components/Home/OfferSection.js
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "../../styles/offer.module.css";
 import Image from "next/image";
-import warenty from '../../assets/Images/warenty.png'
-import happy from '../../assets/Images/happy.png'
-import architect from '../../assets/Images/architect.png'
-import designer from '../../assets/Images/designer.png'
-import city from '../../assets/Images/globe.png'
-import emi from '../../assets/Images/emi.png'
-import project from '../../assets/Images/project.png'
-import expr from '../../assets/Images/experience.png'
+import warenty from "../../assets/Images/warenty.png";
+import happy from "../../assets/Images/happy.png";
+import architect from "../../assets/Images/architect.png";
+import designer from "../../assets/Images/designer.png";
+import city from "../../assets/Images/globe.png";
+import emi from "../../assets/Images/emi.png";
+import project from "../../assets/Images/project.png";
+import expr from "../../assets/Images/experience.png";
+import check from "../../assets/Images/check.png";
 
 function OfferSection() {
   return (
@@ -18,39 +19,148 @@ function OfferSection() {
       <div className={styles.slidernew}>
         <div className={styles.slide_track}>
           <div className={styles.slides}>
-           <div className={styles.offerImageContainer}><Image src={warenty} alt="err" width={60} className={styles.offerImages}/></div> 
+            <div className={styles.offerImageContainer}>
+              <Image
+                src={warenty}
+                alt="err"
+                width={60}
+                className={styles.offerImages}
+              />
+            </div>
+            <Image
+              src={check}
+              alt="err"
+              width={40}
+              className={styles.checkIcon}
+            />
             <div>5 Years Warenty</div>
           </div>
           <div className={styles.slides}>
-          <div className={styles.offerImageContainer}> <Image src={happy} alt="err" width={60} className={styles.offerImages}/></div>
+            <div className={styles.offerImageContainer}>
+              {" "}
+              <Image
+                src={happy}
+                alt="err"
+                width={60}
+                className={styles.offerImages}
+              />
+            </div>
+            <Image
+              src={check}
+              alt="err"
+              width={40}
+              className={styles.checkIcon}
+            />
             <div>Happy Customer</div>
           </div>
           <div className={styles.slides}>
-         <div className={styles.offerImageContainer}> <Image src={architect} alt="err" width={60} className={styles.offerImages}/></div>
+            <div className={styles.offerImageContainer}>
+              {" "}
+              <Image
+                src={architect}
+                alt="err"
+                width={60}
+                className={styles.offerImages}
+              />
+            </div>
+            <Image
+              src={check}
+              alt="err"
+              width={40}
+              className={styles.checkIcon}
+            />
             <div>1000+ Architect</div>
           </div>
           <div className={styles.slides}>
-            <div className={styles.offerImageContainer}> <Image src={designer} alt="err" width={60} className={styles.offerImages}/></div>
+            <div className={styles.offerImageContainer}>
+              {" "}
+              <Image
+                src={designer}
+                alt="err"
+                width={60}
+                className={styles.offerImages}
+              />
+            </div>
+            <Image
+              src={check}
+              alt="err"
+              width={40}
+              className={styles.checkIcon}
+            />
             <div>3500+ Designer</div>
           </div>
           <div className={styles.slides}>
-            <div className={styles.offerImageContainer}> <Image src={city} alt="err" width={60} className={styles.offerImages}/></div>
+            <div className={styles.offerImageContainer}>
+              {" "}
+              <Image
+                src={city}
+                alt="err"
+                width={60}
+                className={styles.offerImages}
+              />
+            </div>
+            <Image
+              src={check}
+              alt="err"
+              width={40}
+              className={styles.checkIcon}
+            />
             <div>100+ cities</div>
           </div>
           <div className={styles.slides}>
-            <div className={styles.offerImageContainer}> <Image src={emi} alt="err" width={60} className={styles.offerImages}/></div>
+            <div className={styles.offerImageContainer}>
+              {" "}
+              <Image
+                src={emi}
+                alt="err"
+                width={60}
+                className={styles.offerImages}
+              />
+            </div>
+            <Image
+              src={check}
+              alt="err"
+              width={40}
+              className={styles.checkIcon}
+            />
             <div>EMI Facility</div>
           </div>
           <div className={styles.slides}>
-            <div className={styles.offerImageContainer}> <Image src={project} alt="err" width={60} className={styles.offerImages}/></div>
+            <div className={styles.offerImageContainer}>
+              {" "}
+              <Image
+                src={project}
+                alt="err"
+                width={60}
+                className={styles.offerImages}
+              />
+            </div>
+            <Image
+              src={check}
+              alt="err"
+              width={40}
+              className={styles.checkIcon}
+            />
             <div>500+ Projects</div>
           </div>
           <div className={styles.slides}>
-            <div className={styles.offerImageContainer}> <Image src={expr} alt="err" width={60} className={styles.offerImages}/></div>
+            <div className={styles.offerImageContainer}>
+              {" "}
+              <Image
+                src={expr}
+                alt="err"
+                width={60}
+                className={styles.offerImages}
+              />
+            </div>
+            <Image
+              src={check}
+              alt="err"
+              width={40}
+              className={styles.checkIcon}
+            />
             <div>12+ year of Experience</div>
           </div>
-
-         
         </div>
       </div>
     </div>
