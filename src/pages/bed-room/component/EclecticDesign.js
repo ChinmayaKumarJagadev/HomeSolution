48ba3ad0b62c90a806808cb6c563395fc49a11c7
chinmaya-elred <chinmaya@elred.io>
2024-09-21 17:41:30 +0530

    'push'

--- src/pages/bed-room/component/EclecticDesign.js
+++ src/pages/bed-room/component/EclecticDesign.js
@@ -14,40 +14,40 @@ import Image from "next/image";
 function EclecticRoomDesign() {
   const data = [
     {
-      title: "An Eclectic Bedroom with Vibrant Patterns and Artistic Decor",
+      title: "An Traditional Bedroom with Vibrant Patterns and Artistic Decor",
       image: Image1,
       description:
-        "This eclectic bedroom showcases a mix of vibrant patterns and artistic decor, creating a visually captivating and unique space. Bold patterns on the bedding and curtains add depth and character, while artful decor pieces, such as abstract wall art and creative accessories, enhance the room’s distinct style. The combination of diverse elements results in a dynamic and inviting atmosphere that reflects a love for creativity and individual expression."
+        "This Traditional bedroom showcases a mix of vibrant patterns and artistic decor, creating a visually captivating and unique space. Bold patterns on the bedding and curtains add depth and character, while artful decor pieces, such as abstract wall art and creative accessories, enhance the room’s distinct style. The combination of diverse elements results in a dynamic and inviting atmosphere that reflects a love for creativity and individual expression."
     },
     {
-      title: "An Eclectic Bedroom with Bold Colors and Unique Furnishings",
+      title: "An Traditional Bedroom with Bold Colors and Unique Furnishings",
       image: Image2,
       description:
-        "This eclectic bedroom embraces bold colors and unique furnishings to create a lively and personal space. The use of vibrant hues, like deep blues and bright yellows, adds energy and character. Unique furnishings, including vintage finds and handcrafted pieces, contribute to the room’s eclectic charm. The blend of colorful textiles and distinctive furniture ensures a visually engaging and comfortable bedroom that stands out."
+        "This Traditional bedroom embraces bold colors and unique furnishings to create a lively and personal space. The use of vibrant hues, like deep blues and bright yellows, adds energy and character. Unique furnishings, including vintage finds and handcrafted pieces, contribute to the room’s Traditional charm. The blend of colorful textiles and distinctive furniture ensures a visually engaging and comfortable bedroom that stands out."
     },
     {
-      title: "Eclectic Bedroom with a Wooden Bed Frame and Colorful Decor",
+      title: "Traditional Bedroom with a Wooden Bed Frame and Colorful Decor",
       image: Image3,
       description:
-        "This eclectic bedroom features a wooden bed frame that brings a touch of rustic warmth to the space. The natural texture of the wood contrasts beautifully with colorful decor elements, such as patterned pillows and vibrant rugs. The mix of traditional wooden furniture and eclectic decorations creates a harmonious and inviting bedroom that blends comfort with artistic flair."
+        "This Traditional bedroom features a wooden bed frame that brings a touch of rustic warmth to the space. The natural texture of the wood contrasts beautifully with colorful decor elements, such as patterned pillows and vibrant rugs. The mix of traditional wooden furniture and Traditional decorations creates a harmonious and inviting bedroom that blends comfort with artistic flair."
     },
     {
-      title: "An Eclectic Bedroom with Open Closet and Mixed Patterns",
+      title: "An Traditional Bedroom with Open Closet and Mixed Patterns",
       image: Image4,
       description:
-        "This eclectic bedroom includes an open closet area, adding functionality and style. The room features a mix of patterns, from geometric prints to florals, layered across bedding, rugs, and wall decor. The open closet design enhances the spacious feel of the room, while the eclectic mix of patterns and colors creates a lively and personalized space."
+        "This Traditional bedroom includes an open closet area, adding functionality and style. The room features a mix of patterns, from geometric prints to florals, layered across bedding, rugs, and wall decor. The open closet design enhances the spacious feel of the room, while the Traditional mix of patterns and colors creates a lively and personalized space."
     },
     {
-      title: "An Eclectic Bedroom with Playful Textiles and Quirky Decor",
+      title: "An Traditional Bedroom with Playful Textiles and Quirky Decor",
       image: Image5,
       description:
-        "This eclectic bedroom is characterized by playful textiles and quirky decor, resulting in a vibrant and whimsical space. The room features a variety of patterns, from stripes to florals, on cushions, bedding, and curtains. Quirky decor pieces, such as unique wall art and creative accents, add to the room’s fun and lively atmosphere. The eclectic mix of patterns and playful details ensures a space full of personality and charm."
+        "This Traditional bedroom is characterized by playful textiles and quirky decor, resulting in a vibrant and whimsical space. The room features a variety of patterns, from stripes to florals, on cushions, bedding, and curtains. Quirky decor pieces, such as unique wall art and creative accents, add to the room’s fun and lively atmosphere. The Traditional mix of patterns and playful details ensures a space full of personality and charm."
     },
     {
-      title: "An Eclectic Bedroom with a Cozy Seating Area and Artistic Touches",
+      title: "An Traditional Bedroom with a Cozy Seating Area and Artistic Touches",
       image: Image6,
       description:
-        "This eclectic bedroom includes a cozy seating area that adds comfort and style to the space. Artistic touches, such as colorful paintings and eclectic furnishings, infuse the room with character. The seating area provides a perfect spot for relaxation, while the overall design blends eclectic elements and cozy comfort, making it a unique and inviting retreat."
+        "This Traditional bedroom includes a cozy seating area that adds comfort and style to the space. Artistic touches, such as colorful paintings and Traditional furnishings, infuse the room with character. The seating area provides a perfect spot for relaxation, while the overall design blends Traditional elements and cozy comfort, making it a unique and inviting retreat."
     }
   ];
   
@@ -87,7 +87,7 @@ function EclecticRoomDesign() {
   return (
     <div className={styles.livingRoomDesignContainer}>
       <div className={styles.modernLivingRoom}>
-        Our Eclectic Bed Room Design
+        Our Traditional Bed Room Design
       </div>
 
       <div className={styles.scrollWrapper}>
